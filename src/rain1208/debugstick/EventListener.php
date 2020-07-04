ef63a7d4243de5068ec46c3d4be641b83f185fa5
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-07-05 02:52:25 +0900

    Added a function to prevent continuous hitting

--- src/rain1208/debugstick/EventListener.php
+++ src/rain1208/debugstick/EventListener.php
@@ -6,11 +6,14 @@ use pocketmine\block\Block;
 use pocketmine\block\BlockIds;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\Player;
 
 
 class EventListener implements Listener
 {
+    private $data;
 
     public function InteractEvent(PlayerInteractEvent $event)
     {
@@ -21,7 +24,8 @@ class EventListener implements Listener
         }
     }
 
-    private function changeData(Player $player,Block $block) {
+    private function changeData(Player $player,Block $block)
+    {
         $meta = $block->getDamage();
         switch ($block->getId()) {
             case BlockIds::WOOD:
@@ -35,11 +39,33 @@ class EventListener implements Listener
             case BlockIds::ENDER_CHEST:
             case BlockIds::WALL_SIGN:
                 $block->setDamage(($meta+1 <= 4)?$meta+1:0);
+                $player->sendActionBarMessage("向きを変更しました");
                 break;
             case BlockIds::SIGN_POST:
                 $block->setDamage(($meta+1<=15)?$meta+1:0);
+                $player->sendActionBarMessage("向きを変更しました");
             /*default:
                 $player->sendActionBarMessage($block->getName()."はプロパティを持っていません");*/
+        }
+    }
+
+    //連打防止
+    public function onReceive(DataPacketReceiveEvent $event)
+    {
+        $player = $event->getPlayer();
+        if ($player->getInventory()->getItemInHand()->getNamedTag()->hasTag("debug")) {
+            $name = $player->getName();
+            $packet = $event->getPacket();
+            $time = ceil(microtime(true)*1000);
+            if ($packet instanceof InventoryTransactionPacket) {
+                if (!isset($this->data[$name])) {
+                    $this->data[$name] = $time;
+                } else if ($time - $this->data[$name] >= 500) {
+                    $this->data[$name] = $time;
+                } else {
+                    $event->setCancelled();
+                }
+            }
         }
     }
 }
